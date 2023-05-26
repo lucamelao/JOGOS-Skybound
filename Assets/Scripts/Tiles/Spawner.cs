ada453ebe837d143f1ae5cb3a7e8e975939c83c6
pauloschade <paulosc4@al.insper.edu.br>
2023-05-25 23:27:40 -0300

    infinite map

--- Assets/Scripts/Tiles/Spawner.cs
+++ Assets/Scripts/Tiles/Spawner.cs
@@ -7,15 +7,53 @@ namespace Tiles
 {
   public class Spawner : MonoBehaviour
   {
-      [SerializeField] private GameObject tile;
+      [SerializeField] private List<GameObject> modules;
       [SerializeField] private int xPos;
-
       [SerializeField] private int yPos;
+
+      [SerializeField] private int maxCapacity = 5;
+
+      private float timePassed = 0.0f;
+      private float timeToSpawn = 10.0f;
+      private Queue<GameObject> spawnedModules;
+
+     
       //[SerializeField] private GameObject parent;
       void Start()
       {
-        Instantiate(tile, new Vector3(xPos, yPos, 0), Quaternion.identity);
+        spawnedModules = new Queue<GameObject>(maxCapacity);
+        for(int i = 0; i < maxCapacity; i++)
+        {
+          GameObject newTile = Instantiate(modules[0], new Vector3(xPos + i * 20, yPos, 0), Quaternion.identity);
+          Debug.Log("COUNT: " + spawnedModules.Count);
+          spawnedModules.Enqueue(newTile);
+        }
+        // Instantiate(modules[0], new Vector3(xPos, yPos, 0), Quaternion.identity);
         //newTile.transform.position += tile.transform;
+      }
+
+      void FixedUpdate()
+      {
+        //check time passed
+        //if time passed, spawn new module
+        //if spawnedModules.Count > maxCapacity, destroy oldest module
+        //if spawnedModules.Count < maxCapacity, spawn new module
+        //if spawnedModules.Count == maxCapacity, do nothing
+        //Debug.Log("COUNT: " + spawnedModules.Count);
+        timePassed += Time.deltaTime;
+        if(timePassed >= timeToSpawn)
+        {
+          SpawnModule();
+          timePassed = 0.0f;
+        }
+      }
+
+      void SpawnModule()
+      {
+        GameObject oldTile = spawnedModules.Dequeue();
+         GameObject newTile = Instantiate(modules[0], new Vector3(xPos + spawnedModules.Count * 20, yPos, 0), Quaternion.identity);
+        Destroy(oldTile);
+        spawnedModules.Enqueue(newTile);
       }
   }
 }
